639c6734fc4bca4743a6e80684b81352441377f5
sdhornet <76028034+sdhornet@users.noreply.github.com>
2023-01-30 14:08:49 -0600

    Output to APPDATA/Roaming/out.log
    I wanted to be able to use this with runas, so now it will write the logins to a file instead of the console window.

--- SavedLogin.cs
+++ SavedLogin.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace SharpChromium
 {
@@ -21,12 +23,35 @@ namespace SharpChromium
 
         public void Print()
         {
+            FileStream ostrm;
+            StreamWriter writer;
+            TextWriter oldOut = Console.Out;
+
+            try
+            {
+                string location = "%APPDATA%/out.log";
+                string str = Environment.ExpandEnvironmentVariables(location);
+                ostrm = new FileStream(str, FileMode.OpenOrCreate, FileAccess.Write);
+                writer = new StreamWriter(ostrm);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Cannot open Redirect.txt for writing");
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            Console.SetOut(writer);
+
             string user = Environment.GetEnvironmentVariable("USERNAME");
             Console.WriteLine("--- Chromium Credential (User: {0}) ---", user);
             Console.WriteLine("URL      : {0}", Url);
             Console.WriteLine("Username : {0}", Username);
             Console.WriteLine("Password : {0}", Password);
             Console.WriteLine();
+            Console.SetOut(oldOut);
+            writer.Close();
+            ostrm.Close();
         }
     }
 }
